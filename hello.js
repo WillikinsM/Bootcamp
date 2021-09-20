d031a01e625ede058e918862d4ad91830e0846e0
WillikinsM <willikins98@gmail.com>
2021-09-20 17:28:33 -0300

    Fixing scroll smoothness

--- hello.js
+++ hello.js
@@ -1,8 +1,39 @@
-var testbutton = document.getElementById("test");
+//var cardClick = document.getElementsByClassName("card-post");
 
-testbutton.addEventListener("click", test);
+//cardClick[0].addEventListener("click", scrollSmooth);
 
-function test() {
-  alert("funfou");
-  console.log("teste funfou");
-}
+//console.log(cardClick);
+
+//function test() {
+//alert("funfou");
+//console.log("teste funfou");
+//}
+
+//function scrollSmooth() {
+  //var info = document.getElementById("javascript-info");
+  //var offsetTop = document.querySelector(href).offsetTop;
+  //console.log(info);
+  //console.log("test");
+
+  //const href = this.getAttribute("href");
+ 
+  //document.querySelector(href).scrollIntoView({
+   // behavior: "smooth"
+
+  //document.getElementById(info).scrollIntoView({
+    //behavior: "smooth",
+ // });
+
+  function scrollSmooth() {
+    //var info = document.getElementById("javascript-info");
+  
+    document.getElementById("javascript-info").scrollIntoView({
+      behavior: "smooth",
+    });
+  
+    //info.scrollIntoView({
+    //behavior: "smooth",
+    //});
+    
+  }
+//}
