d89447c26b5e2acd046e036b7688e508fe6a2fb3
WillikinsM <willikins98@gmail.com>
2021-09-20 19:41:53 -0300

    Suavizado a rolagem da pagina ao clicar nos cards

--- hello.js
+++ hello.js
@@ -1,39 +1,13 @@
-//var cardClick = document.getElementsByClassName("card-post");
+/**
+ * This method smooth the scrooling when a link is clicked
+ * 
+ */
+function scrollSmooth(info) {
+  console.log(info);
+  var offset = document.querySelector("#" + info).offsetTop;
 
-//cardClick[0].addEventListener("click", scrollSmooth);
-
-//console.log(cardClick);
-
-//function test() {
-//alert("funfou");
-//console.log("teste funfou");
-//}
-
-//function scrollSmooth() {
-  //var info = document.getElementById("javascript-info");
-  //var offsetTop = document.querySelector(href).offsetTop;
-  //console.log(info);
-  //console.log("test");
-
-  //const href = this.getAttribute("href");
- 
-  //document.querySelector(href).scrollIntoView({
-   // behavior: "smooth"
-
-  //document.getElementById(info).scrollIntoView({
-    //behavior: "smooth",
- // });
-
-  function scrollSmooth() {
-    //var info = document.getElementById("javascript-info");
-  
-    document.getElementById("javascript-info").scrollIntoView({
-      behavior: "smooth",
-    });
-  
-    //info.scrollIntoView({
-    //behavior: "smooth",
-    //});
-    
-  }
-//}
+  scroll({
+    top: offset - 100,
+    behavior: "smooth",
+  });
+}
