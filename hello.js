a80ff2b2d5fe70106cd7405a5d6c2fcb8123b1f2
WillikinsM <willikins98@gmail.com>
2021-09-21 11:16:30 -0300

    Adicionado bootstrap

--- hello.js
+++ hello.js
@@ -1,9 +1,8 @@
 /**
  * This method smooth the scrooling when a link is clicked
- * 
+ *
  */
 function scrollSmooth(info) {
-  console.log(info);
   var offset = document.querySelector("#" + info).offsetTop;
 
   scroll({
